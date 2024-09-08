288a8af7ab3039a5cd90a35b13f3bf1b1a7911dc
Tobias Berger <toby@tobot.dev>
2024-09-08 17:14:59 +0200

    Update dependencies

--- flake.nix
+++ flake.nix
@@ -42,7 +42,7 @@
               fenix' = (import fenix { inherit system pkgs; });
               toolchain = fenix'.fromToolchainFile {
                 file = ./rust-toolchain.toml;
-                sha256 = "sha256-3jVIIf5XPnUU1CRaTyAiO0XHVbJl12MSx3eucTXCjtE=";
+                sha256 = "sha256-VZZnlyP69+Y3crrLHQyJirqlHrTtGTsyiSnZB8jEvVo=";
               };
             in
             {
